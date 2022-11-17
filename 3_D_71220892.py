8419c05c8434fdc01d8430c41519c8abc352bea2
Thanel <tanellodar84@gmail.com>
2022-11-17 16:20:07 +0700

    Sudah selesai

---
+++ 3_D_71220892.py
@@ -0,0 +1,9 @@
+nama=input("Masukan Nama Anda:")
+p=nama.split(" ")
+d=p[0]
+b=p[1]
+matkul=(input("Masukan Mata Kuliah:"))
+grup=(input("Masukan Grup:"))
+
+print("Haloo!", b, "," , d)
+print(d, "tergabung dalam kelas", matkul, "pada grup", grup)
